08c36999f410a162cb4032e28e40d9e5b323a1b8
Jonathan <hramos30498@gmail.com>
2022-06-07 15:32:25 -0600

    added game function

--- script.js
+++ script.js
@@ -2,10 +2,45 @@ const computerPlay = () => {
     let RPS;
     const randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) {
-        RPS = "Rock";
+        return "rock";
     } else if (randomNumber === 1) {
-        RPS = "Paper";
+        return "paper";
     } else {
-        RPS = "Scissor";
+        return "scissor";
+    }
+}
+
+const playRound = (playerSelection, computerSelection) => {
+    let computerPlay = computerSelection;
+    let playerSelect = playerSelection;
+    let playerPlay = playerSelect.toLowerCase();
+
+    if(computerPlay === "rock" && playerPlay === "paper") {
+        return 'You Win! Paper beats Rock';
+    } else if (computerPlay === "paper" && playerPlay === "paper") {
+        return "It's a Draw!";
+    } else if (computerPlay === "scissor" && playerPlay === "paper") {
+        return 'You Lose! Scissor beats Paper';
+    } else if (computerPlay === "rock" && playerPlay === "rock") {
+        return "It's a Draw!";
+    } else if (computerPlay === "paper" && playerPlay === "rock") {
+        return 'You Lose! Paper beats Rock';
+    } else if (computerPlay === "scissor" && playerPlay === "rock") {
+        return 'You Win! Rock beats Scissor';
+    } else if (computerPlay === "rock" && playerPlay === "scissor") {
+        return 'You Lose! Rock beats Scissor';
+    } else if (computerPlay === "paper" && playerPlay === "scissor") {
+        return 'You Win! Scissor beats Paper';
+    } else if (computerPlay === "scissor" && playerPlay === "scissor") {
+        return "It's a Draw!";
+    }
+}
+
+const game = () => {
+    let computerScore = 0;
+    let playerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let result = playRound(playerSelection, computerSelection);
     }
 }
