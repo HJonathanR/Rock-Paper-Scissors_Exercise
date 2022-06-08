03140f3038bad57bf8d64c72d2947bc03e5de217
Jonathan <hramos30498@gmail.com>
2022-06-08 10:35:34 -0600

    resolve conflict

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorBtn = document.querySelector('#scissor');
+const resultContainer = document.querySelector("#result");
+let playerScore = 0;
+let computerScore = 0;
+let drawScore = 0;
+
 const computerPlay = () => {
     const randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) {
@@ -14,53 +22,89 @@ const computerPlay = () => {
 const playRound = (playerSelection, computerSelection) => {
     let computerPlay = computerSelection;
     let playerPlay = playerSelection.toLowerCase();
-
+    let message;
+    const resultMessage = document.createElement('p');
+    
     if(computerPlay === "rock" && playerPlay === "paper") {
-        return 'You Win! Paper beats Rock';
+        message = 'You Win! Paper beats Rock';
+        resultMessage.textContent = message;
+        resultContainer.appendChild(resultMessage);
     } else if (computerPlay === "paper" && playerPlay === "paper") {
-        return "It's a Draw!";
+        message = "It's a Draw!";
+        resultMessage.textContent = message;
+        resultContainer.appendChild(resultMessage);
     } else if (computerPlay === "scissor" && playerPlay === "paper") {
-        return 'You Lose! Scissor beats Paper';
+        message = 'You Lose! Scissor beats Paper';
+        resultMessage.textContent = message;
+        resultContainer.appendChild(resultMessage);
     } else if (computerPlay === "rock" && playerPlay === "rock") {
-        return "It's a Draw!";
+        message = "It's a Draw!";
+        resultMessage.textContent = message;
+        resultContainer.appendChild(resultMessage);
     } else if (computerPlay === "paper" && playerPlay === "rock") {
-        return 'You Lose! Paper beats Rock';
+        message = 'You Lose! Paper beats Rock';
+        resultMessage.textContent = message;
+        resultContainer.appendChild(resultMessage);
     } else if (computerPlay === "scissor" && playerPlay === "rock") {
-        return 'You Win! Rock beats Scissor';
+        message = 'You Win! Rock beats Scissor';
+        resultMessage.textContent = message;
+        resultContainer.appendChild(resultMessage);
     } else if (computerPlay === "rock" && playerPlay === "scissor") {
-        return 'You Lose! Rock beats Scissor';
+        message = 'You Lose! Rock beats Scissor';
+        resultMessage.textContent = message;
+        resultContainer.appendChild(resultMessage);
+        return message;
     } else if (computerPlay === "paper" && playerPlay === "scissor") {
-        return 'You Win! Scissor beats Paper';
+        message = 'You Win! Scissor beats Paper';
+        resultMessage.textContent = message;
+        resultContainer.appendChild(resultMessage);
     } else if (computerPlay === "scissor" && playerPlay === "scissor") {
-        return "It's a Draw!";
+        message = "It's a Draw!";
+        resultMessage.textContent = message;
+        resultContainer.appendChild(resultMessage);
     }
+
+    return message;
 }
 
-const game = () => {
-    let computerScore = 0;
-    let playerScore = 0;
-    let gameDraws = 0;
 
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("What do you choose?");
-        let computerSelection = computerPlay();
-        let result = playRound(playerSelection, computerSelection);
-        console.log(result);
+rockBtn.addEventListener('click', () => {
+    const playerSelection = "rock";
+    let resultRound = playRound(playerSelection, computerPlay());
+    pointCounter(resultRound);
+});
 
-        if (result.includes("Win")) {
+paperBtn.addEventListener('click', () => {
+    const playerSelection = "paper";
+    let resultRound = playRound(playerSelection, computerPlay());
+    pointCounter(resultRound);
+});
+
+scissor.addEventListener('click', () => {
+    const playerSelection = "scissor";
+    let resultRound = playRound(playerSelection, computerPlay());
+    pointCounter(resultRound);
+});
+
+function pointCounter(resultRound) {
+    const result = resultRound;
+    const winnerText = document.createElement("h2");
+    
+    if(playerScore <= 5 || computerScore <= 5) {
+        if(result.includes('Draw')) {
+            drawScore++;
+        } else if (result.includes('Win')) {
             playerScore++;
-        } else if (result.includes("Lose")) {
-            computerScore++;
         } else {
-            gameDraws++;
+            computerScore++;
         }
     }
 
-    if (playerScore === computerScore) {
-        return "The Final Result is: Draw!";
-    } else if (playerScore > computerScore) {
-        return "The Final Result is: You Win!";
-    } else {
-        return "The Final Result is: You Lose!";
+    if(playerScore === 5) {
+        winnerText.textContent = "You Win the Game!";
+        resultContainer.appendChild(winnerText);
+    } else if (computerScore === 5) {
+        winnerText.textContent = "The Computer wins the Game";
+        resultContainer.appendChild(winnerText);
     }
 }
